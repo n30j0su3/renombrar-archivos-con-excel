4cd776239d2ce6fc6dfec3cb252837886bbfa5e3
n30j0su3 <jorojas1@hotmail.com>
2020-12-17 12:15:38 -0500

    Archivos necesarios para el proceso
    Contiene el script para listar ficheros, el script de ejemplo para renombrar y el template de excel.

---
+++ Bulk_rename_N30.bat
@@ -0,0 +1,4 @@
+REN "Captura - copia (2).PNG" dato-1-valor-1-dato-1-valor-2-dato-1-valor-3.png
+REN "Captura - copia (3).PNG" dato-2-valor-1-dato-2-valor-2-dato-2-valor-3.png
+REN "Captura - copia.PNG" dato-3-valor-1-dato-3-valor-2-dato-3-valor-3.png
+REN Captura.PNG dato-4-valor-1-dato-4-valor-2-dato-4-valor-3.png
